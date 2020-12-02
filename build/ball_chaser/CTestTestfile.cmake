a29f0837e57bcfce8d5a7b2e9587edf69230b734
kyrillos Fekry <kyrlosfekry@gmail.com>
2020-12-03 01:58:38 +0200

    edit map_pkg

--- build/ball_chaser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kronton/localbot_ws/src/ball_chaser
-# Build directory: /home/kronton/localbot_ws/build/ball_chaser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/chaser_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kronton/localbot_ws/src/chaser_robot
-# Build directory: /home/kronton/localbot_ws/build/chaser_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
